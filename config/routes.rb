982b0bf10df3e2b10642acc0705251c3a0766a0f
sakamoto-99 <sakamoto.engineer.99@gmail.com>
2021-07-08 17:55:18 +0900

    新規登録2

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
   root to: "home#index"
 end
